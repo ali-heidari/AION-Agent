b60d6abab7d0414af6f0900192c2de30c3d4caf7
ali-heidari <ali-heidari@outlook.com>
2025-10-26 20:51:18 +0330

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,117 @@
+mod mock;
+mod reward;
+
+use aion_math::math::Math;
+use color_eyre::Result;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
+use std::ops::Div;
+use std::path::Path;
+use std::sync::{Arc, Mutex};
+use std::u32;
+
+use crate::mock::SyntheticState;
+use crate::node::{Node, RunningMode};
+use crate::reward::{compute_reward, compute_reward_with_success};
+
+fn use_tui() -> Result<(), color_eyre::Report> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn get_features(state: &mut SyntheticState, lowest_state: u32) -> Vec<f32> {
+    let features = state.next(mock::Mode::Generative, 0.0, 0.0, lowest_state);
+    features
+}
+
+fn from_csv_dataset(state: &mut SyntheticState) -> Result<()> {
+    let file_path = Path::new("vmCloud_data.csv");
+    let mut rdr = csv::Reader::from_path(file_path)?;
+    let max = 1.0; //if rng.gen_bool(0.04) {1.0} else {0.4};
+    for result in rdr.records() {
+        let record = result?;
+        if record.get(2).unwrap().is_empty() || record.get(3).unwrap().is_empty() {
+            continue;
+        }
+        let max = 1.0; //if rng.gen_bool(0.04) {1.0} else {0.4};
+        let features = state.next(
+            mock::Mode::Inputs,
+            record
+                .get(2)
+                .unwrap()
+                .parse::<f32>()
+                .unwrap()
+                .div(100.0)
+                .clamp(0.05, max),
+            record
+                .get(3)
+                .unwrap()
+                .parse::<f32>()
+                .unwrap()
+                .div(100.0)
+                .clamp(0.05, max),
+            100,
+        );
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+    let state = Arc::new(Mutex::new(SyntheticState::new()));
+    let cloned_state = Arc::clone(&state);
+
+    let mut c = 0;
+    let mut a = vec![0; 3];
+    let mut r = vec![0.0; 3];
+    loop {
+        // break;
+        let features = get_features(&mut cloned_state.lock().unwrap(), u32::MAX);
+        let raw_reward = compute_reward(&features);
+
+        let reward = raw_reward; //((raw_reward + 1.0) / (2.0)).clamp(0.0, 1.0);
+        let state = if features[0] > 0.66 && features[1] > 0.66 {
+            0
+        } else if features[0] > 0.33 && features[1] > 0.33 {
+            1
+        } else {
+            2
+        };
+        a[state] += 1;
+        r[state] = (r[state] + reward) / a[state] as f32;
+        let v = Math::variance_of_ratios(a.iter().map(|x| *x as f32).collect());
+        // println!("[{:?}], {}, {}, {}", features, reward, state, v);
+        c += 1;
+        if c > 1000000 {
+            let sum = a.iter().sum::<i32>() as f32;
+            let x: Vec<f32> = a.iter().map(|x| *x as f32 / sum).collect();
+            println!("#{} => {:?} [{:?}] ({}) [{:?}]", c, a, x, v, r);
+            break;
+        }
+    }
+
+    // Node::start(
+    //     move |lowest_state| get_features(&mut cloned_state.lock().unwrap(), lowest_state),
+    //     RunningMode::Training,
+    // )
+    // .await;
+
+    Ok(())
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Result::Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
+}
---
+++ src/mock.rs
@@ -0,0 +1,117 @@
+use core::f32;
+use sysinfo::System;
+
+const MEMORY_THRESHOLD: f32 = 0.8;
+
+pub enum Mode{
+    SystemMetrics,
+    Inputs,
+    Generative
+}
+
+#[derive(Debug, Clone)]
+pub struct SyntheticState {
+    pub cpu: f32,
+    pub mem: f32,
+    pub swap: f32,
+    pub disk: f32,
+    pub throughput: f32,
+    pub latency: f32,
+    pub direction: f32,// +1 for ramp-up, -1 for ramp-down
+    pub state: u32, 
+    pub count: u32
+}
+
+impl SyntheticState {
+    pub fn new() -> Self {
+        Self {
+            cpu: 0.1,
+            mem: 0.1,
+            swap: 0.05,
+            disk: 0.1,
+            throughput: 0.95,
+            latency: 0.1,
+            direction: 1.0,
+            state:0,
+            count:1
+        }
+    }
+
+    pub fn next(&mut self,mode:Mode, cpu:f32,mem:f32,lowest_state:u32) -> Vec<f32> {
+        use rand::Rng;
+        let mut rng = rand::thread_rng();
+
+let mut range = match lowest_state {
+    // 0 => (0.7,1.0),
+    // 1 => (0.35,0.7),
+    // 2 => (0.05,0.35),
+    _ => (0.01,1.0)
+
+};
+
+// range=if rng.gen_bool(0.5){(0.05,1.0)} else {range};
+// self.state=lowest_state;
+// self.count+=1;
+        //  println!("lowest_state: {}, range: {:?}",lowest_state,range);
+
+
+        // Flip direction sometimes
+        if rng.gen_bool(0.5) {
+            // 2% chance to switch ramp direction
+            self.direction *= -1.0;
+        } 
+
+        // CPU & memory ramp
+        // First we update all information of our `System` struct.
+    //    let mut sys = System::new_all();
+    //    sys.refresh_all();
+        self.cpu = 
+        // cpu;
+        //    sys.global_cpu_usage() / 100.0; 
+        (self.cpu + self.direction * rng.gen_range(0.01..0.03)).clamp(range.0,range.1);
+        
+        self.mem = 
+        // mem;
+            // sys.used_memory() as f32 / sys.total_memory() as f32;
+         (self.mem + self.direction * rng.gen_range(0.005..0.01)).clamp(range.0, range.1);
+
+        // Swap rises if memory high
+        self.swap =  
+            // sys.used_swap() as f32 / sys.total_swap() as f32;
+            f32::max(0.0, self.mem + rng.gen_range(0.0..0.2) - MEMORY_THRESHOLD);
+        //(self.swap * 0.9 + if self.mem > 0.8 { 0.02 } else { -0.01 }).clamp(0.0, 0.3);
+
+        // Disk random variation with light correlation
+        let alpha = 0.3; // CPU contribution to disk
+        let beta = 0.4; // Memory contribution to disk
+        self.disk =
+            (alpha * self.cpu + beta * self.mem + rng.gen_range(-0.05..0.05)).clamp(0.0, 1.0);
+        // self.disk = (self.disk * 0.8 + self.cpu * 0.2 + rng.gen_range(-0.05..0.05)).clamp(0.0, 1.0);
+
+        // Throughput inversely related to load
+        let gamma = 0.5; // Impact of CPU/memory on throughput
+        self.throughput = (1.0 - (self.cpu + self.mem) / 2.0).clamp(0.0, 1.0);
+        self.throughput = (self.throughput / 1.0) * (1.0 - gamma * f32::max(self.cpu, self.mem));
+
+        // Latency grows with load
+        let delta = 0.5; // Impact of CPU/memory on latency
+        self.latency = (self.cpu * 0.6 + self.mem * 0.5).clamp(0.0, 1.0);
+        self.latency = f32::min(
+            1.0,
+            (self.latency/1.0) * (1.0 + delta * f32::max(self.cpu, self.mem)),
+        );
+
+        if (self.cpu == range.0 && self.mem==range.0) ||(self.cpu == range.1 && self.mem==range.1){
+            self.direction *= -1.0;
+        }
+
+        vec![
+            self.cpu,
+            self.mem,
+            self.swap,
+            self.disk,
+            self.throughput,
+            self.latency,
+        ]
+    }
+}
