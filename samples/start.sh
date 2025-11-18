7dd39acc6c5c43f68a4500923f9953e83dbac150
ali-heidari <ali-heidari@outlook.com>
2025-11-19 03:25:19 +0330

    Add optional argument to mock busy behavior of agent

--- samples/start.sh
+++ samples/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Start the aion-agent in the background
-/usr/local/bin/aion-agent &
+/usr/local/bin/aion-agent -- $2 &
 
 
 # parameters to know if we need to start mock.py
-if [ "$1" == "busy" ]; then
-    echo "Starting mock agent..."
-    python3 /usr/local/bin/mock.py &
-fi
+# if [ "$1" == "busy" ]; then
+#     echo "Starting mock agent..."
+#     python3 /usr/local/bin/mock.py &
+# fi
 
 # Start the Python server in the foreground
 # This must be the final command and run in the foreground (no &)
